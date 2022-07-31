3db116e19ff079dd6bb00d307c85a958a16afedd
Uzunov Dmitry <duyz@ya.ru>
2022-07-31 18:19:10 +0300

    Использую unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,27 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'worked' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """тест нового посителя"""
+
+    def setUp(self):
+        """установка"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """демонтаж"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        """тест: можно начать список и получить его позже"""
+        self.browser.get('http://localhost:8000')
+
+        # Заголовок и шапка страницы говорят о списках неотложных дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+
+        # Предлагается ввести элелемент списка...
+
+if __name__ == '__main__':
+    unittest.main()
