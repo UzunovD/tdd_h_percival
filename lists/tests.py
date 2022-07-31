eafe7e229c04deb1f305438f543ba4ae2ca87a3b
Uzunov Dmitry <duyz@ya.ru>
2022-07-31 21:13:41 +0300

    Первый модульный тест и разрешение url, фиктивное представление

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
+class HomePageTest(TestCase):
+    """Тест домашней страницы"""
+    
+    def test_root_url_resolves_to_home_page_view(self):
+        """Тест: url '/' преобразуется в представление home_page"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
-class SmokeTest(TestCase):
-    """Тест на токсичность"""
-
-    def test_math(self):
-        """Тест: Неправильные математические рассчеты"""
-        self.assertEqual(1 + 1, 3)
